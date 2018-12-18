4e8eb22d8a0e1b73d5d22f12c8775e6cb9821920
gizemdibekoglu <dibekoglugizem@gmail.com>
2018-12-18 15:53:23 +0300

    ödev1

---
+++ ödev1/ödev1/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace ödev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dOSYAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kAPATToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dOSYAToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1070, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // dOSYAToolStripMenuItem
+            // 
+            this.dOSYAToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kAPATToolStripMenuItem});
+            this.dOSYAToolStripMenuItem.Name = "dOSYAToolStripMenuItem";
+            this.dOSYAToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.dOSYAToolStripMenuItem.Text = "DOSYA";
+            // 
+            // kAPATToolStripMenuItem
+            // 
+            this.kAPATToolStripMenuItem.Name = "kAPATToolStripMenuItem";
+            this.kAPATToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.kAPATToolStripMenuItem.Text = "KAPAT";
+            this.kAPATToolStripMenuItem.Click += new System.EventHandler(this.kAPATToolStripMenuItem_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(916, 38);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(916, 101);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(916, 168);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(349, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(268, 232);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(721, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "KIRMIZI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(721, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "YEŞİL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(721, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "MAVİ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(713, 212);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(713, 294);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(718, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "KAMERA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(743, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ARDUİNO";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(699, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "BAĞLAN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(792, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "DURDUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(746, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "BAĞLAN";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 375);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dOSYAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kAPATToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ödev1/ödev1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Drawing.Imaging;
+using AForge.Imaging;
+using AForge;
+using AForge.Video.DirectShow;
+using AForge.Video;
+using AForge.Math;
+using AForge.Math.Geometry;
+using AForge.Imaging.Filters;
+using System.Windows.Forms;
+
+namespace ödev1
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection CaptureDevice; //aygıtları görüntüleme
+        private VideoCaptureDevice FinalFrame;  //kullanılacak kamerayı secme
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap video1, video2;
+        int R, G, B;
+        int x, y;
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = 255;
+            trackBar1.Minimum = 0;
+            trackBar1.SmallChange = 1;
+            trackBar1.LargeChange = 1;
+            trackBar1.TickFrequency = 1;
+            label1.Text = "KIRMIZI: " + trackBar1.Value.ToString();
+            R = trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            trackBar2.Maximum = 255;
+            trackBar2.Minimum = 0;
+            trackBar2.SmallChange = 1;
+            trackBar2.LargeChange = 1;
+            trackBar2.TickFrequency = 1;
+            label2.Text = "YEŞİL: " + trackBar2.Value.ToString();
+            G = trackBar2.Value;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            trackBar3.Maximum = 255;
+            trackBar3.Minimum = 0;
+            trackBar3.SmallChange = 1;
+            trackBar3.LargeChange = 1;
+            trackBar3.TickFrequency = 1;
+            label3.Text = "MAVİ: " + trackBar3.Value.ToString();
+            B = trackBar3.Value;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (FinalFrame.IsRunning)
+            {
+                FinalFrame.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //kamera başlaması için gerekli kod satırı
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_newFrame);
+           
+            FinalFrame.NewFrame += FinalFrame_newFrame;
+            FinalFrame.Start();
+
+        }
+        //kameradan alınan görüntünün işleme kısmı
+        private void FinalFrame_newFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            video1 = (Bitmap)eventArgs.Frame.Clone();
+            video2 = (Bitmap)eventArgs.Frame.Clone();
+            
+
+
+            Mirror filter2 = new Mirror(false, true);
+            filter2.ApplyInPlace(video1);
+
+
+
+
+
+            EuclideanColorFiltering filter = new EuclideanColorFiltering();
+            filter.CenterColor = new RGB(Color.FromArgb(R, G, B));
+            filter.Radius = 100;
+            filter.ApplyInPlace(video1);
+            BitmapData objectsData = video1.LockBits(new Rectangle(0, 0, video1.Width, video1.Height), ImageLockMode.ReadWrite, video1.PixelFormat);
+            Grayscale grifiltresi = new Grayscale(0.2125, 0.714, 0.0721);//gri filtresi
+            UnmanagedImage griresim = grifiltresi.Apply(new UnmanagedImage(objectsData));
+            video1.UnlockBits(objectsData);
+            BlobCounter blobcounter = new BlobCounter();//birleşik pikselleri numaralandırma işlemi ,nesne sayma
+            blobcounter.MinWidth = 18;//sayılan nesnelerin min genişliği
+            blobcounter.MinHeight = 18;//yuksekliği
+            blobcounter.FilterBlobs = true;//sartlara uyanın işaretlenmesi 
+            blobcounter.ObjectsOrder = ObjectsOrder.Size;//boyutlara göre sıralamaya sokma işlemi 
+
+
+            blobcounter.ProcessImage(video1);//videodaki  bloblar sayılıp işleniyor
+            Rectangle[] rects = blobcounter.GetObjectsRectangles();//blobcounter içindeki blobların verileri alınıyor
+            Blob[] blobs = blobcounter.GetObjectsInformation();
+            pictureBox2.Image = video2;
+            foreach (Rectangle recs in rects)
+            {
+                Graphics g = Graphics.FromImage(video1);
+                if (rects.Length > 0)
+                {
+                    Rectangle objectRect = rects[0];
+                    x = objectRect.X + (objectRect.Width / 2);
+                    y = objectRect.Y + (objectRect.Height / 2);
+                    if (x < 213 && y < 160)
+                    {
+                        serialPort1.Write("1");
+                    }
+                    if ((213 < x && x < 426) && y < 160)
+                    {
+
+                        serialPort1.Write("2");
+                    }
+                    if (426 < x && x < 640 && y < 160)
+                    {
+                        serialPort1.Write("3");
+                    }
+                    if ((x < 213 && (160 < y && y < 320)))
+                    {
+                        serialPort1.Write("4");
+                    }
+                    if ((213 < x && x < 426) && (160 < y && y < 320))
+                    {
+                        serialPort1.Write("5");
+                    }
+                    if ((426 < x && x < 639) && (160 < y && y < 320))
+                    {
+                        serialPort1.Write("6");
+                    }
+                    if (x < 213 && (320 < y && y < 480))
+                    {
+                        serialPort1.Write("7");
+                    }
+                    if ((213 < x && x < 426) && (320 < y && y < 480))
+                    {
+                        serialPort1.Write("8");
+                    }
+                    if ((426 < x && x < 639) && (320 < y && y < 480))
+                    {
+                        serialPort1.Write("9");
+                    }
+
+                    
+                    using (Pen pen = new Pen(Color.FromArgb(252, 3, 26), 2))
+                    {
+                        g.DrawRectangle(pen, objectRect);
+
+                    }
+
+                    
+                    g.DrawString(x.ToString() + "x" + y.ToString(), new Font("Arial", 12), Brushes.Red, new System.Drawing.Point(x, y));
+                    g.Dispose();
+
+                }
+            }pictureBox1.Image = video1;
+        }
+       public void cisimbul(Bitmap video1)
+        {
+
+
+        }     
+
+                
+private void AÇIK_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox2.SelectedIndex = 0;
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+      
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        //kameradan alınan 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //arduino
+            comboBox2.DataSource = SerialPort.GetPortNames();
+            serialPort1.PortName = comboBox2.SelectedItem.ToString();
+            //kamera
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo Device in CaptureDevice)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void kAPATToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (FinalFrame.IsRunning)
+            {
+                FinalFrame.Stop();
+                serialPort1.Write("0");
+                this.Close();
+            }
+        }
+    }
+}
